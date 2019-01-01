f99099dbd28d58d4130fde49e145329e0947695f
Igor Savin <beixiaofeng@gmail.com>
2019-01-01 19:40:07 +0100

    created protected route

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="chat container">
-    <h2>Chat</h2>
+    <h2>Chat {{ this.name }}</h2>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Chat',
+  props: ['name'],
+
   date(){
     return{
 
